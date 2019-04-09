3e183f3fa03c3c815f42058c818b1b322aa7cf0e
Luis Molina <lmolinabe@gmail.com>
2019-04-08 22:16:29 -0600

    Add indentation to make the 'create table' code more readable

--- scripts/InitialSetupScript.sql
+++ scripts/InitialSetupScript.sql
@@ -10,9 +10,9 @@ GO
 --Create the Users table in TestApp database
 IF OBJECT_ID('Users') IS NULL
 CREATE TABLE dbo.Users(
-	Id INT IDENTITY(1,1) PRIMARY KEY
-	,Name VARCHAR(128)
+	 Id		INT IDENTITY(1,1) PRIMARY KEY
+	,Name	VARCHAR(128)
 	,Avatar VARBINARY(MAX)
-	,Email VARCHAR(256)	
+	,Email	VARCHAR(256)
 );
 GO
